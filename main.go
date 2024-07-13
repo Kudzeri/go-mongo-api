bd06986d62fbc282abd7737ab3913128e37c521a
Kudzeri <kudzeri@gmail.com>
2024-07-13 14:08:30 +0500

    setup: router, MongoDB, UserController

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"github.com/Kudzeri/go-mongo-api/config"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
 
+func main() {
+	r := httprouter.New()
+	uc := controllers.NewUserController(config.GetClient())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user/:id", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+
+	log.Fatal(http.ListenAndServe(":9000", r))
 }
