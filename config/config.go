37fd1187f406a3fac9c1277daa496533271753f0
Kudzeri <kudzeri@gmail.com>
2024-07-13 14:03:30 +0500

    add config.go

---
+++ config/config.go
@@ -0,0 +1,18 @@
+package config
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
+
+func GetClient() *mongo.Client {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return client
+}
